41f85611836a6aefb5b4b5e1b8756e8d65303f88
Swarna Roy <39148748+swarnaBD2112@users.noreply.github.com>
2023-09-27 01:04:18 +0600

    first commit

---
+++ app/src/main/java/com/example/myhelloapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myhelloapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //create reference of username and pass and btn
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //check successful login
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin") ){
+
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL", Toast.LENGTH_SHORT ).show();
+                }else {
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED!!!", Toast.LENGTH_SHORT ).show();
+
+                }
+            }
+        });
+
+    }
+}
